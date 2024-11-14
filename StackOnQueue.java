7f27d28b5950dc46e3d63847f537853d11a38f08
Sukhanov Dmitry <79788948+Rapira16@users.noreply.github.com>
2024-11-14 15:43:27 +0300

    Create StackOnQueue.java

---
+++ StackOnQueue.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackOnQueue {
+    private Queue<Integer> queue1;
+    private Queue<Integer> queue2;
+
+    public StackOnQueue() {
+        queue1 = new LinkedList<>();
+        queue2 = new LinkedList<>();
+    }
+
+    // Помещает элемент x на вершину стека
+    public void push(int x) {
+        queue2.offer(x);
+        while (!queue1.isEmpty()) {
+            queue2.offer(queue1.poll());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+
+    // Удаляет элемент на вершине стека и возвращает его
+    public int pop() {
+        return queue1.poll();
+    }
+
+    // Возвращает элемент на вершине стека
+    public int top() {
+        return queue1.peek();
+    }
+
+    // Возвращает true, если стек пуст, в ином случае false
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+
+    // Возвращает строковое представление всех элементов стека
+    public String toString() {
+        return queue1.toString();
+    }
+}
